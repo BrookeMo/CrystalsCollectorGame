f54fb584ac0b8b0dfe3bc8f960431ef79c25f4ca
Brooke Mortensen <brookeamymo@gmail.com>
2018-02-12 03:55:47 -0700

    added on clicks with adding scores

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,35 +1,42 @@
 $(document).ready(function() {
 
-var randomnumber = 0;
 var totalscore = 0;
 var wins = 0;
 var losses = 0;
 
-// generate a random number at the beginning of a round
 // function called: startRound
+function startRound() {
+// reset totalscore to zero
+    var totalscore === 0;
 // generate a random number between 19 and 120
     var goal = Math.floor(Math.random() * 120) + 19;
 // insert the generated number in the p tag in the randomnumber div
-        // $(".randomnumber").html(randomnumber);
+    $("#randomnumber").html(goal);
 // generate 4 random numbers to assign to the crystals
-    var blueNum = Math.floor(Math.random() * 20) +1;
-    var greenNum = Math.floor(Math.random() * 20) +1;
-    var orangeNum = Math.floor(Math.random() * 20) +1;
-    var pinkNum = Math.floor(Math.random() * 20) +1;
-// do the same for blue orange and pink
-// onclick crystal, it 0 + random number =
+    var greenNum = Math.floor(Math.random() * 15) +1;
+    var blueNum = Math.floor(Math.random() * 15) +1;
+    var orangeNum = Math.floor(Math.random() * 15) +1;
+    var pinkNum = Math.floor(Math.random() * 15) +1;
+// onclick crystal, it totalscore + colorNum = new totalscore
         $("#greencrystal").on("click", function() {
-
+            console.log(totalscore);
+            var totalscore = totalscore + greenNum;
+            console.log(totalscore);
         });
         $("#bluecrystal").on("click", function() {
-
+            var totalscore = totalscore + blueNum;
+            console.log(totalscore);
         });
         $("#orangecrystal").on("click", function() {
-
+            var totalscore = totalscore + orangeNum;
+            console.log(totalscore);
         });
         $("#pinkcrystal").on("click", function() {
-
+            var totalscore = totalscore + pinkNum;
+            console.log(totalscore);
         });
+};
+startRound();
 // display the = in the total score div
         // $("#totalscore").html(totalscore);
     // if the equals is < the randomnumber, continue gameplay
