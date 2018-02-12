aef3749d30b6255cde76a28f133415e5ada34cf9
Brooke Mortensen <brookeamymo@gmail.com>
2018-02-12 02:21:17 -0700

    javascript psuedocode added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,3 +2,16 @@ var randomnumber = 0;
 var totalscore = 0;
 var wins = 0;
 var losses = 0;
+
+// generate a random number at the beginning of a round
+// function called: startRound
+// generate a random number between 19 and 120
+// insert the generated number in the p tag in the randomnumber div
+// generate a random number to assign to greencrystal
+// do the same for blue orange and pink
+// onclick crystal, it 0 + random number =
+// display the = in the total score div
+    // if the equals is < the randomnumber, continue gameplay
+    // if the equals is = the randomnumber, + 1 to wins and restart game
+    // if the equals i > the randomnumber, + 1 to losses and restart game
+    
