4650abcb5984b519b5df1db40d4b618ed6c49528
Brooke Mortensen <brookeamymo@gmail.com>
2018-02-12 12:28:08 -0700

    complete first attempt at javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,7 +7,7 @@ var losses = 0;
 // function called: startRound
 function startRound() {
 // reset totalscore to zero
-    var totalscore === 0;
+    var totalscore = 0;
 // generate a random number between 19 and 120
     var goal = Math.floor(Math.random() * 120) + 19;
 // insert the generated number in the p tag in the randomnumber div
@@ -20,27 +20,36 @@ function startRound() {
 // onclick crystal, it totalscore + colorNum = new totalscore
         $("#greencrystal").on("click", function() {
             console.log(totalscore);
-            var totalscore = totalscore + greenNum;
+            totalscore = totalscore + greenNum;
             console.log(totalscore);
         });
         $("#bluecrystal").on("click", function() {
-            var totalscore = totalscore + blueNum;
+            totalscore = totalscore + blueNum;
             console.log(totalscore);
         });
         $("#orangecrystal").on("click", function() {
-            var totalscore = totalscore + orangeNum;
+            totalscore = totalscore + orangeNum;
             console.log(totalscore);
         });
         $("#pinkcrystal").on("click", function() {
-            var totalscore = totalscore + pinkNum;
+            totalscore = totalscore + pinkNum;
             console.log(totalscore);
         });
 };
 startRound();
 // display the = in the total score div
-        // $("#totalscore").html(totalscore);
+        $("#totalscore").html(totalscore);
     // if the equals is < the randomnumber, continue gameplay
-    // if the equals is = the randomnumber, + 1 to wins and restart game
-    // if the equals i > the randomnumber, + 1 to losses and restart game
-
+        // if the equals is = the randomnumber, + 1 to wins and restart game
+        if (totalscore = randomnumber){
+            wins = wins + 1;
+            $("#wins").htnl(wins);
+            startRound();
+        };
+        // if the equals i > the randomnumber, + 1 to losses and restart game
+        if (totalscore > randomnumber){
+            losses = losses + 1;
+            $("#losses").html(losses);
+            startRound();
+        };
 });
