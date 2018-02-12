090405c6f3a00c2bfd9b8c3044147a8529be6128
Brooke Mortensen <brookeamymo@gmail.com>
2018-02-12 02:49:35 -0700

    added jquery link

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,6 +7,7 @@ var losses = 0;
 // function called: startRound
 // generate a random number between 19 and 120
 // insert the generated number in the p tag in the randomnumber div
+        // $(".randomnumber").html(randomnumber);
 // generate a random number to assign to greencrystal
 // do the same for blue orange and pink
 // onclick crystal, it 0 + random number =
@@ -14,4 +15,3 @@ var losses = 0;
     // if the equals is < the randomnumber, continue gameplay
     // if the equals is = the randomnumber, + 1 to wins and restart game
     // if the equals i > the randomnumber, + 1 to losses and restart game
-    
